258f2b98c6c87ea344731e9c18108c2ad4672fef
Aditya Kulkarni <adi.kul30@gmail.com>
2017-11-16 17:06:57 +0530

    Firebase Storage usage added

--- app/src/main/java/kulkarni/aditya/firebaserecyclerdemo/MainActivity.java
+++ app/src/main/java/kulkarni/aditya/firebaserecyclerdemo/MainActivity.java
@@ -1,5 +1,11 @@
 package kulkarni.aditya.firebaserecyclerdemo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,13 +14,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -23,17 +39,21 @@ public class MainActivity extends AppCompatActivity {
     // Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
-
-    // Firebase instance variables
+    private FirebaseStorage mFirebaseStorage;
+    private StorageReference storageRef;
+    private StorageReference profilesRef;
     private DatabaseReference mFirebaseDatabaseReference;
     private FirebaseRecyclerAdapter<Courses, MainActivity.CoursesViewHolder>
             mFirebaseAdapter;
 
+    private static final int READ_REQUEST_CODE = 1337;
     private static final String TAG = "MainActivity";
-    public static final String ANONYMOUS = "anonymous";
+    private static final String ANONYMOUS = "anonymous";
     private String mUsername;
     private RecyclerView mMessageRecyclerView;
     private LinearLayoutManager mLinearLayoutManager;
+    private ImageView profileImage;
+    private Uri downloadUrl;
 
 
     public static class CoursesViewHolder extends RecyclerView.ViewHolder {
@@ -51,42 +71,99 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mUsername = ANONYMOUS;
+        setReferences();
+
+        profileImage = (ImageView) findViewById(R.id.profileImage);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                startActivityForResult(intent, READ_REQUEST_CODE);
+
+            }
+        });
 
         mMessageRecyclerView = (RecyclerView) findViewById(R.id.course_recycler_view);
         mLinearLayoutManager = new LinearLayoutManager(this);
-//        mLinearLayoutManager.setStackFromEnd(true);
         mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
 
-        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
         mFirebaseAdapter = new FirebaseRecyclerAdapter<Courses, CoursesViewHolder>(Courses.class,R.layout.course_layout_item,CoursesViewHolder.class,mFirebaseDatabaseReference.child("courses")) {
             @Override
             protected void populateViewHolder(CoursesViewHolder viewHolder, Courses model, int position) {
-                Log.d("coursename","sdf");
                 viewHolder.courseName.setText(model.name);
             }
         };
 
-        mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-            @Override
-            public void onItemRangeInserted(int positionStart, int itemCount) {
-                super.onItemRangeInserted(positionStart, itemCount);
-                int count = mFirebaseAdapter.getItemCount();
-//                int lastVisiblePosition =
-//                        mLinearLayoutManager.findLastCompletelyVisibleItemPosition();
-//                // If the recycler view is initially being loaded or the
-//                // user is at the bottom of the list, scroll to the bottom
-//                // of the list to show the newly added message.
-//                if (lastVisiblePosition == -1 ||
-//                        (positionStart >= (count - 1) &&
-//                                lastVisiblePosition == (positionStart - 1))) {
-//                    mMessageRecyclerView.scrollToPosition(positionStart);
-//                }
-            }
-        });
-
         mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
         mMessageRecyclerView.setAdapter(mFirebaseAdapter);
 
+    }
+
+    public void setReferences(){
+        mUsername = ANONYMOUS;
+        mFirebaseStorage = FirebaseStorage.getInstance();
+        storageRef = mFirebaseStorage.getReference();
+
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                //Toast.makeText(this,uri.toString(),Toast.LENGTH_LONG).show();
+
+                uploadImage(uri);
+                Toast.makeText(MainActivity.this,"Upload started !",Toast.LENGTH_SHORT).show();
+
+
+            }
+            else Toast.makeText(this,"Some Error !",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void uploadImage(Uri uriFromFile){
+        //Calculate hash for a unique value
+//        int imageHash = uriFromFile.hashCode();
+//        profilesRef = storageRef.child("profiles").child(String.valueOf(imageHash));
+
+        profilesRef = storageRef.child("profiles");
+
+        UploadTask uploadTask = profilesRef.putFile(uriFromFile);
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.d("downloadUrl","unsuccessful");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                downloadUrl = taskSnapshot.getDownloadUrl();
+                Log.d("downloadUrl",String.valueOf(downloadUrl));
+                Toast.makeText(MainActivity.this,"Upload successful !",Toast.LENGTH_SHORT).show();
+                downloadImage(downloadUrl);
+            }
+        });
+    }
+
+    public void downloadImage(Uri downloadUri){
+        // Reference to an image file in Firebase Storage
+        StorageReference httpsReference = mFirebaseStorage.getReferenceFromUrl(downloadUri.toString());
+
+        // Load the image using Glide
+        Glide.with(this /* context */)
+                .using(new FirebaseImageLoader())
+                .load(httpsReference)
+                .into(profileImage);
     }
 }
